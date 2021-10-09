8bb858cf590205c05a78ff1c06f38dee89cde964
irxne19 <68653473+irxne19@users.noreply.github.com>
2021-10-09 17:28:53 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar -->    
+<nav class="navbar navbar-expand-md navbar-dark .bg-dark.bg-gradient" style="background-color:black;">
+<a class="navbar-brand" href="#"><img src="img/icon.png" height="40" width="40"></a>
+      <a class="navbar-brand" href="index.php">BRICKFIELD BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+                
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </ul>
+          </div>
+       </nav>
+
